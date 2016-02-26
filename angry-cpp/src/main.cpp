7783c6be05ffb1512db4b3a918d8fc24176a2182
Patryk Wertka <spartanpageagain@gmail.com>
2016-02-26 15:31:28 +0100

    Adjusted hello_world

--- angry-cpp/src/main.cpp
+++ angry-cpp/src/main.cpp
@@ -6,23 +6,31 @@
 #include "scenes/scene.hpp"
 #include "scenes/scenes-manager.hpp"
 #include "scenes/scenes-runner.hpp"
+#include "input/mouse.hpp"
 
 struct empty_scene: public angrycpp::scene {
     virtual void init() override {};
     virtual void update() override {};
-    virtual void draw(angrycpp::graphics::renderer &) override {};
+    virtual void draw(angrycpp::graphics::renderer &renderer) override {
+        int x, y, width = 50, height = 50;
+        std::tie(x, y) = angrycpp::input::mouse::pos();
+
+        renderer.begin(SDL_Color{ 0, 0, 0 });
+        renderer.draw(SDL_Rect{ x-width, y-height, width*2, height*2 }, SDL_Color{ 255, 255, 255 });
+        renderer.end();
+    };
     virtual void finish() override {};
 };
 
 #undef main
 int main(int argc, const char* argv[]) {
     angrycpp::game::core::guard game_guard{};
-    angrycpp::window window(320, 320, "Hello world!");
+    angrycpp::window window(640, 640, "Hello world!");
 
     angrycpp::scenes_manager scenes_manager;
     scenes_manager.scenes.push_back(std::make_unique<empty_scene>());
 
-    angrycpp::scenes_runner{ scenes_manager }.run(angrycpp::graphics::renderer{});
+    angrycpp::scenes_runner{ scenes_manager }.run(angrycpp::graphics::renderer{ window });
 
     return run_tests(argc, argv);
 }
